e9223ffa444d334eba1ae65454e73abda0ae999f
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-03-31 13:58:55 -0300

    Update attacher.php

--- resources/config/attacher.php
+++ resources/config/attacher.php
@@ -1,10 +1,24 @@
 <?php
-
+# config/attacher.php
 return [
-    'model'    => 'Artesaos\Attacher\AttacherModel',
-    'base_url' => '',
-    'path'     => '/uploads/images/:id/:style/:filename',
-    'styles'   => [
+	'model'    => 'Artesaos\Attacher\AttacherModel', # You can customize the model for your needs.
+    'base_url' => '', # The url basis for the representation of images.
+    'path'     => '/uploads/images/:id/:style/:filename', # Change the path where the images are stored.
 
+    # Where the magic happens.
+    # This is where you record what the "styles" that will apply to your image.
+    # Each style takes as the parameter is one \Intervention\Image\Image
+    # See more in http://image.intervention.io/
+    'styles'   => [
+        # Generate thumb (?x500)
+        'thumb'=> function($image) 
+        {
+        	$image->resize(null, 500, function ($constraint) {
+        	    $constraint->aspectRatio();
+        	    $constraint->upsize();
+        	});
+        
+        	return $image;
+        }
     ]
 ];
