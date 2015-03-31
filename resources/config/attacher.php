b00aa10c9592c84ef5dfb5fda737b3273bfaeed5
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-03-31 14:00:04 -0300

    fix tabs

--- resources/config/attacher.php
+++ resources/config/attacher.php
@@ -1,10 +1,11 @@
 <?php
 # config/attacher.php
+
 return [
-	'model'    => 'Artesaos\Attacher\AttacherModel', # You can customize the model for your needs.
+    'model'    => 'Artesaos\Attacher\AttacherModel', # You can customize the model for your needs.
     'base_url' => '', # The url basis for the representation of images.
     'path'     => '/uploads/images/:id/:style/:filename', # Change the path where the images are stored.
-
+    
     # Where the magic happens.
     # This is where you record what the "styles" that will apply to your image.
     # Each style takes as the parameter is one \Intervention\Image\Image
@@ -13,12 +14,12 @@ return [
         # Generate thumb (?x500)
         'thumb'=> function($image) 
         {
-        	$image->resize(null, 500, function ($constraint) {
-        	    $constraint->aspectRatio();
-        	    $constraint->upsize();
-        	});
-        
-        	return $image;
+            $image->resize(null, 500, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+            });
+            
+            return $image;
         }
     ]
 ];
