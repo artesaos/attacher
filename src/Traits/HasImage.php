a82335153ccad89d87be32256dbfd93f1ac9c8f8
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-04-01 23:30:57 -0300

    HasImage trait

---
+++ src/Traits/HasImage.php
@@ -0,0 +1,38 @@
+<?php namespace Artesaos\Attacher\Traits;
+
+use Artesaos\Attacher\AttacherModel;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+trait HasImage
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
+     */
+    public function image()
+    {
+        return $this->morphOne(config('attacher.model'), 'imageable');
+    }
+
+    /**
+     * @param UploadedFile $image
+     *
+     * @return AttacherModel
+     */
+    public function addImage(UploadedFile $image)
+    {
+        $instance = $this->createImageModel();
+        $instance->setupFile($image);
+
+        $this->image()->save($instance);
+
+        return $instance;
+    }
+
+    /**
+     * @return AttacherModel
+     */
+    protected function createImageModel()
+    {
+        return $this->image()->getRelated()->newInstance();
+    }
+}
